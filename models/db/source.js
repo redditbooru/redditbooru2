a322511452d802ef69288e8d84a89a2770340aec
Matt Hackmann <matt.hackmann@gmail.com>
2014-01-11 15:32:18 -0800

    Initial commit

---
+++ models/db/source.js
@@ -0,0 +1,25 @@
+var _ = require('underscore'),
+    util = require('util'),
+    Dal = require('../../lib/dal.js'),
+    Source = function(obj) {
+        if (_.isObject(obj)) {
+            Source.copyRowFromDb(this, obj);
+        }
+    };
+
+// Database mapping
+Source._dbTable = 'images';
+Source._primaryKey = 'id';
+Source._dbMap = {
+    id: 'source_id',
+    name: 'source_name',
+    baseUrl: 'source_baseurl',
+    type: 'source_type',
+    enabled: 'source_enabled',
+    subdomain: 'source_subdomain',
+    contentRating: 'source_content_rating'
+};
+
+_.extend(Source, Dal);
+
+module.exports = Source;
