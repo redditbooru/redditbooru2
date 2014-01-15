98382cf2685a4b87ce67eb9f7e3f375effe93cd2
Matt Hackmann <matt.hackmann@gmail.com>
2014-01-14 20:36:28 -0800

    Action queue added

--- cron.js
+++ cron.js
@@ -3,31 +3,91 @@ var _ = require('underscore'),
     Source = require('./models/db/source.js'),
     Post = require('./models/db/post.js'),
     Image = require('./models/db/image.js'),
-    reddit = require('./lib/reddit.js');
+    reddit = require('./lib/reddit.js'),
 
-Source.query([ { col: 'enabled', val: true }, { col: 'type', val: 'subreddit' } ]).then(function(rows) {
+    /**
+     * Constants
+     */
+    ACTION_UPDATE = 'update',
+    ACTION_CREATE = 'create',
 
-    _.each(rows, function(source) {
+    // Queue of things to act upon
+    actionQueue = [],
 
-        reddit
-            .getDataListing(source.name)
-            .then(function(data) {
-                
-                if (_.has(data, 'data') && _.has(data.data, 'children')) {
-                    _.each(data.data.children, function(post) {
-                        Post
-                            .createFromRedditPost(post.data)
-                            .then(function(blah) {
-                                console.log(blah);
-                            })
-                            .fail(function(err) {
-                                console.log(err);
+    /**
+     * Retrieves reddit data for later updates
+     */
+    getRedditData = function() {
+
+        Source.query([ { col: 'enabled', val: true }, { col: 'type', val: 'subreddit' } ]).then(function(rows) {
+
+            _.each(rows, function(source) {
+
+                reddit
+                    .getDataListing(source.name)
+                    .then(function(data) {
+                        
+                        if (_.has(data, 'data') && _.has(data.data, 'children')) {
+                            _.each(data.data.children, function(post) {
+                                Post
+                                    .createFromRedditPost(post.data)
+                                    .then(function(post) {
+                                        
+                                        // Check for an existing post with this reddit ID
+                                        Post.query([ { col: 'externalId', val: post.externalId } ]).then(function(row) {
+                                            if (!row.length) {
+                                                post.sync().then(function() {
+                                                    actionQueue.push({ action:ACTION_CREATE, data:post });
+                                                });
+                                            } else {
+                                                actionQueue.push({ 
+                                                    action:ACTION_UPDATE,
+                                                    data:{
+                                                        post: post,
+                                                        row: row[0]
+                                                    }
+                                                });
+                                            }
+                                        });
+
+                                    })
+                                    .fail(function(err) {
+                                        console.log(err);
+                                    });
                             });
+                        }
+
                     });
-                }
 
             });
 
-    });
+        });
+    
+    },
 
-});
+    /**
+     * Acts on items in the action Queue
+     */
+    queueRunner = function() {
+
+        while (actionQueue.length > 0) {
+
+            var item = actionQueue.shift();
+            switch (item.action) {
+                case ACTION_CREATE:
+                case ACTION_UPDATE:
+                    // Update the database
+                    item.data.post.id = item.data.row.id;
+                    item.data.row.sync();
+                    break;
+            }
+
+        }
+
+        // Revisit in a couple seconds
+        setTimeout(queueRunner, 2000);
+
+    };
+
+getRedditData();
+queueRunner();
