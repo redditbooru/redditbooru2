49f68003c6365396f6e80cf52ee7a8c5e4624a22
Matt Hackmann <matt.hackmann@gmail.com>
2014-01-20 11:37:14 -0800

    'post_id' removed in favor of xref table, constructor for creating an image object from an image URL

--- models/db/image.js
+++ models/db/image.js
@@ -1,18 +1,23 @@
 var _ = require('underscore'),
     util = require('util'),
+    defer = require('q').defer,
     Dal = require('../../lib/dal.js'),
+    ImageProcessor = require('../../lib/ImageProcessor.js'),
     Image = function(obj) {
         if (_.isObject(obj)) {
             Image.copyRowFromDb(this, obj);
         }
     };
 
+// Inherit Dal
+util.inherits(Image, Dal);
+_.extend(Image, Dal);
+
 // Database mapping
 Image._dbTable = 'images';
 Image._dbPrimaryKey = 'id';
 Image._dbMap = {
     id: 'image_id',
-    postId: 'post_id',
     sourceId: 'source_id',
     url: 'image_url',
     cdnUrl: 'image_cdn_url',
@@ -34,6 +39,33 @@ Image._dbMap = {
     contentRating: 'image_rating'
 };
 
-_.extend(Image, Dal);
+/**
+ * Creates an image object with histogram data from a URL
+ */
+Image.createFromUrl = function(url) {
+    var retVal = defer();
+
+    ImageProcessor.processFromUrl(url).then(function(image) {
+        var obj = {
+                image_url: url,
+                image_width: image.width,
+                image_height: image.height
+            },
+            i = 0;
+
+        for (; i < 4; i++) {
+            obj['image_hist_r' + (i + 1)] = image.histogram.red[i];
+            obj['image_hist_g' + (i + 1)] = image.histogram.green[i];
+            obj['image_hist_b' + (i + 1)] = image.histogram.blue[i];
+        }
+
+        retVal.resolve(new Image(obj));
+
+    }).fail(function(err) {
+        retVal.reject(err);
+    });
+
+    return retVal.promise;
+};
 
 module.exports = Image;
