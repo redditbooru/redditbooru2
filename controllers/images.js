a5d929d23015ecb470e510aaa02247d8a1a890c5
Matt Hackmann <matt.hackmann@gmail.com>
2014-01-26 16:43:07 -0800

    Extra options

--- controllers/images.js
+++ controllers/images.js
@@ -9,16 +9,19 @@ exports.api = function(req, res) {
             sources: q.sources || 1, // default to awwnime; don't want to be serving up porn to everybody by default
             postId: q.postId || null,
             externalId: q.externalId || null,
-            limit: q.limit || 25
+            limit: q.limit || 25,
+            afterDate: q.afterDate || null,
+            minDate: q.minDate || null,
+            maxDate: q.maxDate || null,
+            user: q.user || null,
+            imageUrl: q.imageUri || null // retaining old name for BC
         },
         retVal = null,
         start = Date.now();
-
-    res.set('Content-Type', 'text/javascript');
 
     Item.query(options).then(function(results) {
         response.writeJSON(results, true);
     }).fail(function(err) {
         response.writeJSON({ err: true, msg: err }, true);
-    });    
+    });
 };
