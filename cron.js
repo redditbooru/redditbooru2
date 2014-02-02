8a364f941aaae4895b19784711c8a422d7336c4c
Matt Hackmann <matt.hackmann@gmail.com>
2014-02-01 23:40:13 -0800

    Spun queue code off into a library

--- cron.js
+++ cron.js
@@ -1,6 +1,7 @@
 global._db = require('./lib/db.js');
 var _ = require('underscore'),
     fs = require('fs'),
+
     Source = require('./models/db/source.js'),
     Post = require('./models/db/post.js'),
     Image = require('./models/db/image.js'),
@@ -9,6 +10,7 @@ var _ = require('underscore'),
     reddit = require('./lib/reddit.js'),
     ImageResolver = require('./lib/ImageResolver.js'),
     mongo = require('./lib/mongo.js'),
+    QueueRunner = require('./lib/QueueRunner'),
 
     /**
      * Constants
@@ -19,22 +21,22 @@ var _ = require('underscore'),
     TIMEOUT_GET_DATA = 60000, // Fetch reddit data once every two minutes
     TIMEOUT_GET_DATA_SHORT = 5000, // Amount of milliseconds to wait for the queue to ease up
     TIMEOUT_RUN_QUEUE = 100, // Queue should be checked every tenth of a second
-    MAX_ACTIVE_ITEMS = 100, // Maximum number of items allowed to be in an active state before the queue can advance
+    MAX_ACTIVE_ITEMS = 50, // Maximum number of items allowed to be in an active state before the queue can advance
     UPDATE_TIME_THRESHOLD = 600, // Maximum number of seconds to wait before updating a post
     UPDATE_SCORE_THRESHOLD = 10, // Score difference to kick off post update
 
-    // Queue of things to act upon
-    actionQueue = [],
-    activeItems = 0,
-    oldActiveItems = 0,
-    oldQueueLength = 0,
+    // The queue runner object
+    queueRunner = null,
+    _async = null, // shorthand for queueRunner.async
+
+    // This is a list of new items being worked on that can be used to save data integrity in case of a failure
+    newPosts = [],
 
     _log = fs.createWriteStream('out.log', { flags:'a' }),
 
-    logActionComplete = function(logHead, message) {
+    log = function(logHead, message) {
         _log.write(logHead + ' ' + message + '\n');
         console.log(logHead + ' ' + message);
-        activeItems--;
     },
 
     /**
@@ -42,90 +44,66 @@ var _ = require('underscore'),
      */
     getSourcesData = function() {
 
-        activeItems++;
         Source.query([ { col: 'enabled', val: true }, { col: 'type', val: 'subreddit' } ]).then(function(rows) {
-            logActionComplete('[main]', 'Finished retrieving list of active subs');
+            log('[main]', 'Finished retrieving list of active subs');
             _.each(rows, function(source) {
 
                 // Throw the update into the queue
-                actionQueue.push({
-                    action: ACTION_GET_DATA,
-                    data: {
-                        source: source,
-                        endpoint: source.name + '/new/'
-                    }
+                queueRunner.add(ACTION_GET_DATA, {
+                    source: source,
+                    endpoint: source.name + '/new/'
                 });
 
             });
 
         }).fail(function(err) {
-            logActionComplete('[main]', 'Unable to get active subs: ' + err);
+            log('[main]', 'Unable to get active subs: ' + err);
         });
-
-        setTimeout(getSourcesData, TIMEOUT_GET_DATA);
 
     },
 
     getRedditData = function(subreddit, source, period) {
         var logHead = '[reddit/' + subreddit + ']';
-        console.log(' -- Getting post data for ' + subreddit + ' -- ');
+        log(' -- Getting post data for ' + subreddit + ' -- ');
 
-        activeItems++;
-        reddit
-            .getDataListing(subreddit, 100, period)
-            .then(function(data) {
+        _async(reddit.getDataListing(subreddit, 100, period)).then(function(data) {
 
-                logActionComplete(logHead, 'Finished fetching data for ' + subreddit);
+            log(logHead, 'Finished fetching data for ' + subreddit);
 
-                if (_.has(data, 'data') && _.has(data.data, 'children')) {
-                    _.each(data.data.children, function(post) {
-                        var author = post.data.author;
-                        Post
-                            .createFromRedditPost(post.data)
-                            .then(function(post) {
+            if (_.has(data, 'data') && _.has(data.data, 'children')) {
+                _.each(data.data.children, function(post) {
+                    var author = post.data.author;
+                    _async(Post.createFromRedditPost(post.data)).then(function(post) {
 
-                                // Check for an existing post with this reddit ID
-                                activeItems++;
-                                Post.query([ { col: 'externalId', val: post.externalId } ]).then(function(row) {
-                                    if (!row.length) {
-                                        logActionComplete(logHead, 'New post: ' + post.title + ' (' + post.externalId + ')');
-                                        console.log('[  NEW  ] ' + post.title);
-                                        post.sourceId = source.id;
-                                        actionQueue.push({ action:ACTION_CREATE, data: {
-                                            post: post,
-                                            source: source,
-                                            author: author
-                                        }});
-                                    } else {
-                                        logActionComplete(logHead, 'Old post: ' + post.title + ' (' + post.externalId + ')');
-                                        actionQueue.push({
-                                            action:ACTION_UPDATE,
-                                            data:{
-                                                post: post,
-                                                row: row[0]
-                                            }
-                                        });
-                                    }
-                                });
+                        // Check for an existing post with this reddit ID
+                        _async(Post.query([ { col: 'externalId', val: post.externalId } ])).then(function(row) {
+                            if (!row.length) {
+                                log(logHead, 'New post: ' + post.title + ' (' + post.externalId + ')');
+                                post.sourceId = source.id;
+                                queueRunner.add(ACTION_CREATE, { post: post, source: source, author: author });
+                            } else {
+                                log(logHead, 'Old post: ' + post.title + ' (' + post.externalId + ')');
+                                queueRunner.add(ACTION_UPDATE, { post: post, row: row[0] });
+                            }
+                        });
 
-                            })
-                            .fail(function(err) {
-                                logActionComplete(logHead, 'Error creating post from reddit data: ' + err);
-                            });
+                    })
+                    .fail(function(err) {
+                        log(logHead, 'Error creating post from reddit data: ' + err);
                     });
-                }
+                });
+            }
 
-            }).fail(function(err) {
-                logActionComplete(logHead, 'Unable to retrieve sub data: ' + err);
-            });
+        }).fail(function(err) {
+            log(logHead, 'Unable to retrieve sub data: ' + err);
+        });
     },
 
     assignImageToPost = function(postId, imageId, logHead) {
-        activeItems++;
-        (new ImagePost({ post_id: postId, image_id: imageId })).sync().then(function(result) {
-            logActionComplete(logHead, 'Image ' + imageId + ' assigned successfully');
+        _async((new ImagePost({ post_id: postId, image_id: imageId })).sync()).then(function(result) {
+            log(logHead, 'Image ' + imageId + ' assigned successfully');
         }).fail(function(err) {
-            logActionComplete(logHead, 'Error syncing image relationship for ' + imageId + ': ' + err);
+            log(logHead, 'Error syncing image relationship for ' + imageId + ': ' + err);
         });
     },
 
@@ -133,73 +111,68 @@ var _ = require('underscore'),
         var source = item.data.source,
             author = item.data.author;
         item.data = item.data.post;
-        console.log(logHead + 'Inserting into database');
-        activeItems++;
-        item.data.sync().then(function(post) {
-            logActionComplete(logHead, 'Insert succeeded. Resolving link to images');
-            activeItems++;
-            ImageResolver.getImageListFromUrl(post.link).then(function(images) {
-                logActionComplete(logHead, '"' + post.link + '" resolved to ' + images.length + ' images');
+        log(logHead, 'Inserting into database');
+
+        _async(item.data.sync()).then(function(post) {
+            log(logHead, 'Insert succeeded. Resolving link to images');
+            _async(ImageResolver.getImageListFromUrl(post.link)).then(function(images) {
+                log(logHead, '"' + post.link + '" resolved to ' + images.length + ' images');
                 _.each(images, function(url) {
                     // Check to see if this image has been loaded already. We want to avoid dupes
-                    activeItems++;
-                    Image.query([ { col: 'url', val: url } ]).then(function(result) {
+                    _async(Image.query([ { col: 'url', val: url } ])).then(function(result) {
                         var imageObj = null;
                         if (!result.length) {
-                            logActionComplete(logHead, 'Retrieving new image ' + url);
-                            activeItems++;
-                            Image.createFromUrl(url).then(function(image) {
-                                logActionComplete(logHead, 'Image ' + url + ' loaded and processed');
-                                activeItems++;
-                                image.sync().then(function(image) {
-                                    logActionComplete(logHead, 'Image ' + url + ' synced to database with ID ' + image.id);
+                            log(logHead, 'Retrieving new image ' + url);
+                            _async(Image.createFromUrl(url)).then(function(image) {
+                                log(logHead, 'Image ' + url + ' loaded and processed');
+                                _async(image.sync()).then(function(image) {
+                                    log(logHead, 'Image ' + url + ' synced to database with ID ' + image.id);
 
                                     // Assign the image to the post
                                     assignImageToPost(post.id, image.id, logHead);
 
                                     // Create the cache item
                                     var item = Item.createItem(post, image, source, author);
-                                    activeItems++;
-                                    mongo.save('posts', item).then(function(result) {
-                                        logActionComplete(logHead, 'Cache item created for ' + image.id);
+                                    _async(mongo.save('posts', item)).then(function(result) {
+                                        log(logHead, 'Cache item created for ' + image.id);
                                     }).fail(function(err) {
-                                        logActionComplete(logHead, 'Error syncing image to mongo cache: ' + err);
+                                        log(logHead, 'Error syncing image to mongo cache: ' + err);
                                     });
                                 }).fail(function(err) {
-                                    logActionComplete(logHead, 'Unable to sync image: ' + err);
+                                    log(logHead, 'Unable to sync image: ' + err);
                                 });
                             }).fail(function(err) {
-                                logActionComplete(logHead, 'Error creating image from ' + url + ': ' + err);
+                                log(logHead, 'Error creating image from ' + url + ': ' + err);
                             });
                         } else {
-                            logActionComplete(logHead, 'Image repurposed from ' + result[0].id);
+                            log(logHead, 'Image repurposed from ' + result[0].id);
                             assignImageToPost(post.id, result[0].id, logHead);
                         }
 
                     }).fail(function(err) {
-                        logActionComplete(logHead, 'Error looking up image URL ' + url + ': ' + err);
+                        log(logHead, 'Error looking up image URL ' + url + ': ' + err);
                     });
                 });
             }).fail(function(err) {
-                logActionComplete(logHead, 'Unable to get information for URL "' + post.link + '": ' + err);
+                log(logHead, 'Unable to get information for URL "' + post.link + '": ' + err);
             });
         }).fail(function(err) {
-            logActionComplete(logHead, 'Error creating post: ' + err);
+            log(logHead, 'Error creating post: ' + err);
         });
     },
 
     updatePostEntry = function(item, logHead) {
         var time = Math.round(Date.now() / 1000);
         if (Math.abs(item.data.row.score - item.data.post.score) > UPDATE_SCORE_THRESHOLD || time > item.data.row.dateUpdated + UPDATE_TIME_THRESHOLD) {
-            console.log(logHead + ' Updating existing item');
+            log(logHead, ' Updating existing item');
             item.data.row.score = item.data.post.score;
             item.data.row.dateUpdated = time;
             item.data.row.keywords = item.data.post.keywords;
-            activeItems++;
-            item.data.row.sync().then(function(result) {
-                logActionComplete(logHead, 'Post updated');
+
+            _async(item.data.row.sync()).then(function(result) {
+                log(logHead, 'Post updated');
             }).fail(function(err) {
-                logActionComplete(logHead, 'Error updating post: ' + err);
+                log(logHead, 'Error updating post: ' + err);
             });
         }
     },
@@ -207,40 +180,34 @@ var _ = require('underscore'),
     /**
      * Acts on items in the action Queue
      */
-    queueRunner = function() {
-        if (actionQueue.length !== oldQueueLength || activeItems !== oldActiveItems) {
-            console.log('-- Processing queue (' + actionQueue.length + ' items, ' + activeItems + ' active) --');
+    queueRunnerCallback = function(item) {
+
+        switch (item.action) {
+            case ACTION_GET_DATA:
+                getRedditData(item.data.endpoint, item.data.source);
+                break;
+            case ACTION_CREATE:
+                createPostEntry(item, '[' + item.data.post.externalId + '] ');
+                break;
+            case ACTION_UPDATE:
+                // Update the database
+                updatePostEntry(item, '[' + item.data.post.externalId + ']');
+                break;
         }
-        while (actionQueue.length > 0 && activeItems < MAX_ACTIVE_ITEMS) {
-
-            var item = actionQueue.shift();
-            switch (item.action) {
-                case ACTION_GET_DATA:
-                    getRedditData(item.data.endpoint, item.data.source);
-                    break;
-                case ACTION_CREATE:
-                    createPostEntry(item, '[' + item.data.post.externalId + '] ');
-                    break;
-                case ACTION_UPDATE:
-                    // Update the database
-                    updatePostEntry(item, '[' + item.data.post.externalId + ']');
-                    break;
-            }
-
-        }
-
-        oldQueueLength = actionQueue.length;
-        oldActiveItems = activeItems;
-
-        // Revisit in a couple seconds
-        setTimeout(queueRunner, TIMEOUT_RUN_QUEUE);
 
     };
 
+queueRunner = new QueueRunner({ delay: TIMEOUT_RUN_QUEUE, limiter: MAX_ACTIVE_ITEMS }, queueRunnerCallback);
+_async = queueRunner.async;
 getSourcesData();
-queueRunner();
+queueRunner.start();
 
 process.on('exit', function() {
+    log('Shutting down');
     _log.close();
-    console.log('Shutting down');
+});
+
+process.on('uncaughtException', function(err) {
+    log('Uncaught exception');
+    log(err.stack);
 });
