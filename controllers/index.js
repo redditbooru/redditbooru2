7404feb9b0188eb72435e52e453bda06aaad0713
Matt Hackmann <matt.hackmann@gmail.com>
2014-02-02 19:33:13 -0800

    basic functionality stubbed

--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,16 @@
 var _ = require('underscore'),
-    Image = require('../models/db/image.js'),
+    Source = require('../models/db/source'),
+    Item = require('../models/cache/item'),
+    Response = require('../lib/Response');
 
     ENTRIES_PER_PAGE = 5;
 
-module.exports = function(app, request, response) {
-
-    var image = Image.query(
-        [ { col: 'postId', val: '15' } ],
-        { postId: 'DESC' },
-        10
-    ).then(function(rows) {
-        console.log(rows);
+module.exports = function(req, res) {
+    var response = new Response(req, res);
+    Source.query([ { col: 'enabled', val: true }, { col: 'type', val: 'subreddit' } ]).then(function(sources) {
+        Item.query({ sources: [ 1 ] }).then(function(images) {
+            response.render('index', { sources: sources, images: JSON.stringify(images) });
+        });
     });
-
-    response.end();
 
 };
